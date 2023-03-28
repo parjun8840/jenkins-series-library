ce046d0fcf809ea77f4517df40385034f4198099
parjun8840 <mailbox.aadya@gmail.com>
2023-03-29 07:05:27 +0900

    adding common dev workflow

---
+++ vars/commonWorkflow.groovy
@@ -0,0 +1,55 @@
+pipeline {
+  agent {
+    kubernetes {
+      yaml '''
+        apiVersion: v1
+        kind: Pod
+        spec:
+          containers:
+          - name: maven
+            image: maven:alpine
+            command:
+            - cat
+            tty: true
+        '''
+    }
+  }
+  stages {
+
+    stage("Checkout Code") {
+               steps {
+                   git branch: 'main',
+                       url: "https://github.com/parjun8840/pipeline-multi-branch"
+               }
+           }
+    stage("Cleaning workspace") {
+               steps {
+                   echo "Cleaning workspace"
+                   sh "mvn -version"
+               }
+           }
+
+    stage("Running Testcase") {
+              steps {
+                   echo "Running Testcase"
+                   sh "mvn -version"
+               }
+          }
+
+    stage("Packing Application") {
+               steps {
+                   echo "Packing Application"
+                   sh "mvn -version"
+               }
+           }
+
+    stage("Build Docker"){
+               steps {
+                   echo "Build Docker"
+                   sh "mvn -version"
+               }
+           }
+
+  
+  }
+}
