64ca7435d4d31d692947176e61a6187554c27faa
parjun8840 <mailbox.aadya@gmail.com>
2023-03-30 07:44:59 +0900

    adding docker build

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -14,15 +14,19 @@ pipeline {
            }
     stage('Docker login') {
          steps {
+            script{
             dockerImage = docker.build("$DOCKER_USR/prometheus:${env.BUILD_TAG}") 
             }
+             }
             }
     stage('Publish') {
     steps {
       echo 'Building and publishing multi-arch image to DockerHub..'
+      script{
       withDockerRegistry([ credentialsId: "DOCKER_ID", url: "" ]) {
         dockerImage.push()
         }
+}
 }
 }
 }
