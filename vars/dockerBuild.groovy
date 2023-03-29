687027f81249f53af4e8e81f7841a7e153fe8dd0
parjun8840 <mailbox.aadya@gmail.com>
2023-03-30 07:02:30 +0900

    adding docker build

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -15,7 +15,8 @@ pipeline {
     stage('Docker login') {
          steps { 
                 echo 'Docker login'
-                sh 'echo $DOCKER_PSW | docker login -u $DOCKER_USR --password-stdin'
+                //sh 'echo $DOCKER_PSW | docker login -u $DOCKER_USR --password-stdin'
+                docker login -u $DOCKER_USR -p $DOCKER_PSW
             }
             }
     stage('Publish') {
