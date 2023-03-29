c4663f7303893275d615b90fbc43c8140e297907
parjun8840 <mailbox.aadya@gmail.com>
2023-03-29 22:24:29 +0900

    adding docker build

---
+++ vars/dockerBuild.groovy
@@ -0,0 +1,43 @@
+def dockerBuild(String repoUrl) {
+pipeline {
+  agent {
+    kubernetes {
+      yaml '''
+        apiVersion: v1
+        kind: Pod
+        spec:
+          containers:
+          - name: docker-build
+            image: docker:20.10.23-cli-alpine3.17
+            command:
+            - cat
+            tty: true
+        '''
+    }
+  }
+  stages {
+    stage("Checkout Code") {
+               steps {
+                   container('docker-build') {
+                   git branch: 'main',
+                       url: "${repoUrl}"
+                      }
+               }
+           }
+    stage('Docker login') {
+         steps {  container('docker-build') {
+                echo 'Docker login'
+                sh 'echo $DOCKER_PSW | docker login -u $DOCKER_USR --password-stdin'
+                }
+            }
+            }
+    stage('Publish') {
+    steps {  container('docker-build') {
+    echo 'Building and publishing multi-arch image to DockerHub..'
+sh 'docker buildx build --push --platform linux/amd64,linux/arm64 -t $DOCKER_ID/prometheus:latest .'
+}
+}
+}
+}
+}
+}
